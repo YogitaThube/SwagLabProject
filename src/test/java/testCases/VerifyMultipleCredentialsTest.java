74e0964b2580ad7aaef1bce1d6a9a3639443ccab
YogitaThube <153998670+YogitaThube@users.noreply.github.com>
2023-12-20 23:17:52 +0530

    Update VerifyMultipleCredentialsTest.java

--- src/test/java/testCases/VerifyMultipleCredentialsTest.java
+++ src/test/java/testCases/VerifyMultipleCredentialsTest.java
@@ -29,7 +29,7 @@ public class VerifyMultipleCredentialsTest extends TestBase
 				{
 							// Username and passworld are given in swag lab page
 					{"standard_user","secret_sauce"},
-					{"locked_out_user","secret_sauce"},
+					//{"locked_out_user","secret_sauce"},
 					{"problem_user","secret_sauce"},
 					{"performance_glitch_user","secret_sauce"},
 					{"error_user","secret_sauce"},
